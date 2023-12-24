c0967e8860f5bbd869af6a4c2b395ea279e5c591
ResumeFriendlyName <thisismygithubthanks@gmail.com>
2023-12-24 22:15:22 +1100

    [RP-1] Styled table pagination

--- src/renderer/src/components/TablePagination.tsx
+++ src/renderer/src/components/TablePagination.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 interface TablePaginationProps {
   offset: number
   amount: number
@@ -7,29 +9,54 @@ interface TablePaginationProps {
 }
 
 const TablePagination = (props: TablePaginationProps): JSX.Element => {
+  const [open, setOpen] = useState<boolean>(false)
+  let pageCount = 1
+
+  if (props.count > 0) {
+    pageCount = Math.max(Math.round(props.count / props.amount), 1)
+  }
+
+  console.log(open)
+
   return (
     <div className="table-pagination">
       <button
+        className="btn btn-md"
         onClick={(): void => props.handleOffset(props.offset - 1)}
         disabled={props.offset === 0}
       >
         Back
       </button>
-      <form>
-        <select
-          name="Pagination amount"
-          value={props.amount}
-          onChange={(e): void => props.handleAmount(Number(e.target.value))}
-        >
-          {/* Testing value 5 */}
-          <option value="5">5</option>
-          <option value="6">10</option>
-          <option value="25">25</option>
-          <option value="50">50</option>
-          <option value="100">100</option>
-        </select>
-      </form>
+
+      <details
+        className="dropdown"
+        open={open}
+        onClick={(e): void => {
+          e.preventDefault()
+          setOpen((prev) => !prev)
+          console.log('ran!')
+        }}
+      >
+        <summary className="btn btn-md">{`Page ${props.offset + 1} of ${pageCount} (${
+          props.amount
+        })`}</summary>
+        <ol className="dropdown-content text-center">
+          {[5, 10, 25, 50, 100].map((value) => (
+            <li
+              key={`li_${value}`}
+              onClick={(e): void => {
+                e.stopPropagation()
+                props.handleAmount(value)
+                setOpen(false)
+              }}
+            >
+              {value}
+            </li>
+          ))}
+        </ol>
+      </details>
       <button
+        className="btn btn-md"
         onClick={(): void => props.handleOffset(props.offset + 1)}
         disabled={(props.offset + 1) * props.amount >= props.count}
       >
