5fcb8d5de0e26f48e0e663a47793a036ca744ddb
ResumeFriendlyName <thisismygithubthanks@gmail.com>
2023-12-29 12:38:27 +1100

    [RP-12] Added info button

---
+++ src/renderer/src/components/InfoButton.tsx
@@ -0,0 +1,31 @@
+import { faInfo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { InfoModal } from './StatusModals'
+import { useState } from 'react'
+
+interface InfoButtonProps {
+  headingText: string
+  content: JSX.Element
+}
+
+const InfoButton = (props: InfoButtonProps): JSX.Element => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  return (
+    <>
+      <button className="btn btn-sm btn-round" onClick={(): void => setOpen(true)}>
+        <FontAwesomeIcon icon={faInfo} />
+      </button>
+      <InfoModal
+        open={open}
+        headingText={props.headingText}
+        content={props.content}
+        handleClose={(): void => {
+          setOpen(false)
+        }}
+      />
+    </>
+  )
+}
+
+export default InfoButton
