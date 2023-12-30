0fcde3c2f09d1e2380a49ce2f53bae7776ea6fab
ResumeFriendlyName <thisismygithubthanks@gmail.com>
2023-12-30 19:43:58 +1100

    [RP-29] Centred loader and fixed whitespace bug

--- src/renderer/src/components/Loader.tsx
+++ src/renderer/src/components/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = (): JSX.Element => {
   return (
-    <div className="absolute flex w-[calc(100%-2rem)] h-full items-center justify-center">
+    <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
       <div className="loader" />
     </div>
   )
