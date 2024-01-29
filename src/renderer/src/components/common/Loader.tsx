a61b634dfc79718ca47cfaf5dcd97e787dd084e0
ResumeFriendlyName <thisismygithubthanks@gmail.com>
2024-01-29 20:42:49 +1100

    [RP-36] Added backdrop  to loader

--- src/renderer/src/components/common/Loader.tsx
+++ src/renderer/src/components/common/Loader.tsx
@@ -1,8 +1,17 @@
-const Loader = (): JSX.Element => {
+import Modal from '../modals/Modal'
+
+interface LoaderProps {
+  open: boolean
+}
+
+const Loader = (props: LoaderProps): JSX.Element => {
   return (
-    <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-      <div className="loader" />
-    </div>
+    // Little "hack" to add backdrop, and remove white background from Modal
+    <Modal open={props.open} className="p-0">
+      <div className="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+        <div className="loader" />
+      </div>
+    </Modal>
   )
 }
 
