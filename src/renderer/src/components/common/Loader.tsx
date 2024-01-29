f789044248d46c31e9607fa400eea5bb26ccbbcf
ResumeFriendlyName <thisismygithubthanks@gmail.com>
2024-01-29 21:20:29 +1100

    [RP-36] Added delay for loading to remove screen flicker

--- src/renderer/src/components/common/Loader.tsx
+++ src/renderer/src/components/common/Loader.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import Modal from '../modals/Modal'
 
 interface LoaderProps {
@@ -5,9 +6,31 @@ interface LoaderProps {
 }
 
 const Loader = (props: LoaderProps): JSX.Element => {
+  const [open, setOpen] = useState<boolean>(props.open)
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout | undefined = undefined
+
+    if (props.open) {
+      // Only show loading screen after 3 seconds so the screen doesn't flicker
+      timeout = setTimeout(() => {
+        if (props.open) {
+          setOpen(true)
+        }
+      }, 3000)
+    } else {
+      setOpen(false)
+    }
+
+    if (timeout !== undefined) {
+      return () => clearTimeout(timeout)
+    }
+    return
+  }, [props.open])
+
   return (
     // Little "hack" to add backdrop, and remove white background from Modal
-    <Modal open={props.open} className="p-0">
+    <Modal open={open} className="p-0">
       <div className="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
         <div className="loader" />
       </div>
