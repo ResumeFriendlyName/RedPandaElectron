1653f14b1ece879141634e3ac989431b6867891d
ResumeFriendlyName <thisismygithubthanks@gmail.com>
2024-09-01 16:28:50 +1000

    RP-33 Transaction table tag rows no longer highlight

--- src/renderer/src/components/tables/TransactionsTable.tsx
+++ src/renderer/src/components/tables/TransactionsTable.tsx
@@ -16,56 +16,54 @@ interface TransactionsTableProps {
 
 const TransactionsTable = (props: TransactionsTableProps): JSX.Element => {
   return props.transactionsWithTags.length ? (
-    <>
-      <table className="table table-hover w-full text-primary-content">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>Balance</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.transactionsWithTags.map((transactionWithTag) => (
-            <Fragment key={`transaction_${transactionWithTag.transaction.id}`}>
-              <tr>
-                <td>{transactionWithTag.transaction.date}</td>
-                <td>{transactionWithTag.transaction.description}</td>
-                <td>{transactionWithTag.transaction.amount.toLocaleString()}</td>
-                <td>{transactionWithTag.transaction.balance.toLocaleString()}</td>
-              </tr>
-              {!props.hideTags && (
-                <tr>
-                  <td colSpan={4}>
-                    <div className="flex justify-start gap-3 flex-wrap">
-                      <AddTagButton
-                        transactionWithTags={transactionWithTag}
-                        handleSelect={(tag: Tag): void =>
-                          props.handleTagAddToTransaction(tag, transactionWithTag.transaction)
+    <table className="table table-hover w-full text-primary-content">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Balance</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.transactionsWithTags.map((transactionWithTag) => (
+          <Fragment key={`transaction_${transactionWithTag.transaction.id}`}>
+            <tr>
+              <td>{transactionWithTag.transaction.date}</td>
+              <td>{transactionWithTag.transaction.description}</td>
+              <td>{transactionWithTag.transaction.amount.toLocaleString()}</td>
+              <td>{transactionWithTag.transaction.balance.toLocaleString()}</td>
+            </tr>
+            {!props.hideTags && (
+              <tr className="hover:!bg-primary">
+                <td colSpan={4}>
+                  <div className="flex justify-start gap-3 flex-wrap">
+                    <AddTagButton
+                      transactionWithTags={transactionWithTag}
+                      handleSelect={(tag: Tag): void =>
+                        props.handleTagAddToTransaction(tag, transactionWithTag.transaction)
+                      }
+                    />
+                    {transactionWithTag.tags.map((tag) => (
+                      <TagChip
+                        key={`td_${transactionWithTag.transaction.id}_${tag.id}_${tag.name}`}
+                        text={tag.name}
+                        onDelete={(): void =>
+                          props.handleTagDeleteWithTransaction(
+                            tag.id,
+                            transactionWithTag.transaction.id
+                          )
                         }
                       />
-                      {transactionWithTag.tags.map((tag) => (
-                        <TagChip
-                          key={`td_${transactionWithTag.transaction.id}_${tag.id}_${tag.name}`}
-                          text={tag.name}
-                          onDelete={(): void =>
-                            props.handleTagDeleteWithTransaction(
-                              tag.id,
-                              transactionWithTag.transaction.id
-                            )
-                          }
-                        />
-                      ))}
-                    </div>
-                  </td>
-                </tr>
-              )}
-            </Fragment>
-          ))}
-        </tbody>
-      </table>
-    </>
+                    ))}
+                  </div>
+                </td>
+              </tr>
+            )}
+          </Fragment>
+        ))}
+      </tbody>
+    </table>
   ) : (
     <div className="info">
       <FontAwesomeIcon icon={faCircleInfo} className="mr-1" />
